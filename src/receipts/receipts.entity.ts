d468ffe54726055b2da2ed733a9af3ecddf6d55b
Loeng Pheaktra <tranet513@gmail.com>
2026-03-19 19:41:31 +0700

    Completed all task

---
+++ src/receipts/receipts.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('receipts')
+export class Receipt {
+  @PrimaryGeneratedColumn('uuid')
+  receiptId: string;
+
+  @Column({ type: 'timestamp' })
+  issuedAt: Date;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ type: 'double precision' })
+  price: number;
+}
